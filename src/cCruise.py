f9809ae45579ae78f7238069ea8fa7b23d9efaae
alma marquez <almamarquez2003@gmail.com>
2023-08-12 18:43:06 -0300

    agrego cositas

--- src/cCruise.py
+++ src/cCruise.py
@@ -3,3 +3,8 @@ class cCruise(cShip):
     def __init__(self, passengers, draft, crew):
         self.passengers = passengers
         cShip.__init__(self, draft, crew)
+    def is_worth_it(self):
+        pesoTotal = self.passengers*2.25+self.crew*1.5
+        worthIt = self.draft-pesoTotal
+        return worthIt
+
--- src/cShip.py
+++ src/cShip.py
@@ -1,4 +1,10 @@
 class cShip(object):
+    cantBarcos=0
     def __init__(self, draft, crew):
         self.draft = draft
         self.crew = crew
+        cShip.cantBarcos = cShip.cantBarcos+1
+
+    def is_worth_it(self):
+        worthIt = self.draft-self.crew*1.5
+        return worthIt
