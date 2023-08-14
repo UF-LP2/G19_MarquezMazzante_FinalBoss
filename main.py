14051b62f451abdf8425fbfba29bb3a2b4d3dc69
Lorenzo Mazzante <lorenzomazzante@hotmail.com.ar>
2023-08-14 16:57:14 -0300

    read file

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import src.cCruise
-import src.cCargo
+import csv
+from src.cShip import cShip
+from src.cCargo import cCargo
+from src.cCruise import cCruise
+
 def main() -> None:
-  print("Hello World")
+  ships = []
+  with open("ships.csv") as file:
+    reader = csv.reader(file)
+
+    for row in reader:
+
+      if (row[2] == "" and row[3] == ""):
+        shipaux = cShip(row[0],row[1])
+        ships.append(shipaux)
+        print("cree un ship")
+
+      elif (row[2]!= "" and row[3] == ""):
+        cruiseaux = cCruise(row[2], row[0], row[1])
+        ships.append((cruiseaux))
+        print("cree un cruise")
+
+      else:
+        cargoaux = cCargo(row[2],row[3],row[0],row[1])
+        ships.append(cargoaux)
+        print("cree un cargo")
+
+
+
 
 
 
 if __name__ == "__main__":
   main()
-
-
-"""Probando si funciona el push(Loren)"""
