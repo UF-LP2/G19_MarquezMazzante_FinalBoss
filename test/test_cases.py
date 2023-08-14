20d681f52fddf3cba59010f1fbd4041d36a03172
alma marquez <almamarquez2003@gmail.com>
2023-08-14 16:13:40 -0300

    unit test bien bien!!!!!!

--- test/test_cases.py
+++ test/test_cases.py
@@ -4,16 +4,16 @@ from src.cCargo import cCargo
 from src.cCruise import cCruise
 
 
-def test_Pruebo():
+def test_Ship():
     Ship = cShip(2000, 300)
     assert Ship.is_worth_it() == 1550
 
 
-def test_pruebodos():
+def test_Cargo():
     Cargo = cCargo(2000, 0.5, 1200, 300)
     assert Cargo.is_worth_it() == -2800
 
 
-def test_pruebatres():
+def test_Cruise():
     Cruise = cCruise(2500, 3000, 1200)
     assert Cruise.is_worth_it() == -4425
